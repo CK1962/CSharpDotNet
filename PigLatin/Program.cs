236d4cdd937f698e63e4b962971f0abc5456d1a8
ck1962 <ckeslin@gmail.com>
2019-05-13 18:13:15 -0500

    corrected the main

--- PigLatin/Program.cs
+++ PigLatin/Program.cs
@@ -14,42 +14,11 @@ namespace PigLatin
             string input = Console.ReadLine();
             /* Translate the word to Pig Latin
             */
-            ToPigLatin_V2(input);
+            ToPigLatin(input);
             Console.Read();
         }
 
-        // ToPigLatin_V1 returns the converted word"
-        public static string ToPigLatin_V1(string word)
-        {
-            char[] vowels = { 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U' };
-
-            int firstVowel = word.IndexOfAny(vowels);
-            if (firstVowel == -1)  // if there is no vowel in the word
-            {
-                return (word + "ay");
-            }
-            else
-            {
-                char firstLetter = word[0]; // get the first letter of the word
-                char lastLetter = word[word.Length - 1]; // get the last letter of the word
-
-                if (vowels.Contains(firstLetter) && vowels.Contains(lastLetter))
-                {
-                    return (word + "yay");
-                }
-                else
-                {
-                    string firstHalf = word.Substring(0, firstVowel);
-                    string secondHalf = word.Substring(firstVowel);
-                    return (secondHalf + firstHalf + "ay");
-                }
-
-            }
-
-        }
-
-        // ToPigLatin_V2 does not return anything"
-        public static void ToPigLatin_V2(string word)
+        public static void ToPigLatin(string word)
         {
             char[] vowels = { 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U' };
 
