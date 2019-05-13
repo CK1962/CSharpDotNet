6a3c2463d463f2e32ae1c75e3b154f7bd69f9555
ck1962 <ckeslin@gmail.com>
2019-05-13 18:02:17 -0500

    finished lesson

---
+++ PigLatin/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PigLatin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter a word that you would like translated to Pig Latin: ");
+            string input = Console.ReadLine();
+            /* Translate the word to Pig Latin
+            */
+            ToPigLatin_V2(input);
+            Console.Read();
+        }
+
+        // ToPigLatin_V1 returns the converted word"
+        public static string ToPigLatin_V1(string word)
+        {
+            char[] vowels = { 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U' };
+
+            int firstVowel = word.IndexOfAny(vowels);
+            if (firstVowel == -1)  // if there is no vowel in the word
+            {
+                return (word + "ay");
+            }
+            else
+            {
+                char firstLetter = word[0]; // get the first letter of the word
+                char lastLetter = word[word.Length - 1]; // get the last letter of the word
+
+                if (vowels.Contains(firstLetter) && vowels.Contains(lastLetter))
+                {
+                    return (word + "yay");
+                }
+                else
+                {
+                    string firstHalf = word.Substring(0, firstVowel);
+                    string secondHalf = word.Substring(firstVowel);
+                    return (secondHalf + firstHalf + "ay");
+                }
+
+            }
+
+        }
+
+        // ToPigLatin_V2 does not return anything"
+        public static void ToPigLatin_V2(string word)
+        {
+            char[] vowels = { 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U' };
+
+            int firstVowel = word.IndexOfAny(vowels);
+
+            if (firstVowel == -1)  // if there is no vowel in the word
+            {
+                Console.WriteLine(word + "ay");
+            }
+            else
+            {
+                char firstLetter = word[0]; // get the first letter of the word
+                char lastLetter = word[word.Length - 1]; // get the last letter of the word
+                if (vowels.Contains(firstLetter) && vowels.Contains(lastLetter))
+                {
+                    Console.WriteLine(word + "yay");
+                }
+                else
+                {
+                    // either the first letter is not a vowel OR the last letter is not a vowel
+                    // split the word into two parts at the first vowel 
+                    string firstHalf = word.Substring(0, firstVowel);
+                    string secondHalf = word.Substring(firstVowel);
+                    Console.WriteLine(secondHalf + firstHalf + "ay");
+                }
+
+            }
+        }
+    }
+}
---
+++ RockPaperScissors/RockPaperScissors.cs
@@ -0,0 +1,101 @@
+﻿using System;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Do you choose rock,paper or scissors");
+            string userChoice = Console.ReadLine();
+
+            //this allows computer to choose random answer
+            Random r = new Random();
+            int computerChoice = r.Next(4);
+
+            //choice 1 if comp and player tie
+            if (computerChoice == 1)
+            {
+                if (userChoice == "rock")
+                {
+                    Console.WriteLine("The computer chose rock");
+                    Console.WriteLine("It is a tie ");
+                }
+                else if (userChoice == "paper")
+                {
+                    Console.WriteLine("The computer chose paper");
+                    Console.WriteLine("It is a tie ");
+
+                }
+                else if (userChoice == "scissors")
+                {
+                    Console.WriteLine("The computer chose scissors");
+                    Console.WriteLine("It is a tie ");
+                }
+                else
+                {
+                    Console.WriteLine("You must choose rock,paper or scissors!");
+
+                }
+
+            }
+
+            //choice 2 if comp wins over player
+            else if (computerChoice == 2)
+            {
+                if (userChoice == "rock")
+                {
+                    Console.WriteLine("The computer chose paper");
+                    Console.WriteLine("Sorry you lose,paper beat rock");
+
+                }
+                else if (userChoice == "paper")
+                {
+                    Console.WriteLine("The computer chose scissors");
+                    Console.WriteLine("Sorry you lose,scissors beat paper ");
+
+                }
+                else if (userChoice == "scissors")
+                {
+                    Console.WriteLine("The computer chose rock");
+                    Console.WriteLine("Sorry you lose,rock beats scissors");
+                }
+                else
+                {
+                    Console.WriteLine("You must choose rock,paper or scissors!");
+                }
+            }
+
+            //choice 3 player wins over comp
+            else if (computerChoice == 3)
+            {
+                if (userChoice == "rock")
+                {
+                    Console.WriteLine("The computer chose scissors");
+                    Console.WriteLine("You win,rock beats scissors");
+
+                }
+                else if (userChoice == "paper")
+                {
+                    Console.WriteLine("The computer chose rock");
+                    Console.WriteLine("You win,paper beats rock");
+
+                }
+                else if (userChoice == "scissors")
+                {
+                    Console.WriteLine("The computer chose paper");
+                    Console.WriteLine("You win,scissors beat paper");
+
+                }
+                else
+                {
+                    Console.WriteLine("You must choose rock,paper or scissors!");
+
+                }
+
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
