d06af45ecf6d89c777d3b6bfb2d3e0fa3ecb1caa
ck1962 <ckeslin@gmail.com>
2019-06-09 19:52:05 -0500

    finished todo code

--- ToDoItem/Program.cs
+++ ToDoItem/Program.cs
@@ -8,20 +8,44 @@ namespace ToDoItem
 {
     class Program
     {
+        static List<ToDoItem> ToDoList;
+
         static void Main(string[] args)
         {
-            string question = "Would you like to add your To Do item?";
-            Console.WriteLine(question);
-            string answer = Console.ReadLine().ToLower();
-                list<ToDoItem> toDoList = new list<ToDoItem>();
-            while (answer != "no")
-                foreach (ToDoItem item in toDoList)
-                    Console.ReadKey();
-            Console.WriteLine("Please tell me the date this item is due");
-            Console.ReadLine();
-            Console.WriteLine("Please give this item a priority(high, normal, low");
+            ToDoList = new List<ToDoItem>();
+            bool keepGoing = true;
+
+            while (keepGoing == true)
+            {
+                string question = "Would you like to keep going or exit?";
+                Console.WriteLine(question);
+                string answer = Console.ReadLine().ToLower();
+                if (answer == "exit")
+                {
+                    keepGoing = false;
+                    continue;
+                }
+                Console.WriteLine("Please give me the description: ");
+                string descriptionInput = Console.ReadLine();
+
+                Console.WriteLine("Please tell me the date this item is due: ");
+                string dateInput = Console.ReadLine();
+
+                Console.WriteLine("Please give this item a priority(high, normal, low: ");
+                string priorityInput = Console.ReadLine();
+
+                ToDoItem obj1 = new ToDoItem(descriptionInput, dateInput, priorityInput);
+                ToDoList.Add(obj1);
+            }
+
+            foreach (ToDoItem item in ToDoList)
+            {
+                Console.WriteLine($"description: {item.Description}; due date: {item.DueDate}; priority: {item.Priority}");
+            }
             Console.ReadLine();
         }
+
+
         class ToDoItem
         {
 
@@ -29,7 +53,7 @@ namespace ToDoItem
             public String DueDate { get; set; }
             public String Priority { get; set; }
 
-            public ToDoItem(string description, string due_date, string priority)
+            public ToDoItem(string description, String due_date, string priority)
             {
                 Description = description;
                 DueDate = due_date;
