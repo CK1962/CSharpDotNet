c8abeaa1f1c86266da4b21d544458f7b53dfb696
ck1962 <ckeslin@gmail.com>
2019-06-24 20:35:02 -0500

    created this file

---
+++ DbContext/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace DbContext
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+         // add the following import to the top for your
+    using Microsoft.EntityFrameworkCore;
+
+
+    class StudentsContext : DbContext
+    {
+
+        // This property corresponds to the table in our database
+        public DbSet<Student> students { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // this tells the framework which file to use as the database
+            optionsBuilder.UseSqlite("Data Source=students.db");
+        }
+    }
+}
+}
