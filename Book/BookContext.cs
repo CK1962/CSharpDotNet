f5122e06ba3803642ea5bcaace9930b2a384adb6
ck1962 <ckeslin@gmail.com>
2019-06-26 20:10:10 -0500

    created book files

---
+++ Book/BookContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using System.IO;
+
+namespace Student
+{
+    class BookContext : DbContext
+    {
+
+        // This property corresponds to the table in our database
+        public DbSet<Book> books { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // get the directory the code is being executed from
+            DirectoryInfo ExeDirectory = new DirectoryInfo(AppContext.BaseDirectory);
+
+            // get the base directory for the project
+            DirectoryInfo ProjectBase = ExeDirectory.Parent.Parent.Parent;
+
+            // add 'students.db' to the project directory
+            String DbFile = Path.Combine(ProjectBase.Title, "book.db");
+
+            // to check what the path of the file is, uncomment the file below
+            Console.WriteLine("using database file :" + DbFile);
+
+            optionsBuilder.UseSqlite("Data Source=" + DbFile);
+        }
+    }
+}
---
+++ Book/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+namespace Book
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BookContext context = new BookContext();
+            context.Database.EnsureCreated();
+
+            // ask the user for a title to add
+            Console.WriteLine("Enter Title and Author of a book: ");
+            String title = Console.ReadLine();
+            string[] nameParts = title.Split();
+
+            Book newBook = new book(nameParts[0], nameParts[1]);
+            context.book.Add(newBook);
+
+        }
+    }
+}
