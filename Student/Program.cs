3f0dec99c22dd87fcfedba1151585c6d5c2291b7
ck1962 <ckeslin@gmail.com>
2019-06-26 19:27:07 -0500

    created and worked on entit framework

---
+++ Student/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Student
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StudentContext context = new StudentContext();
+            context.Database.EnsureCreated();
+
+            // ask the user for a student to add
+            Console.WriteLine("Enter Student full name");
+            String fullName = Console.ReadLine();
+            string[] nameParts = fullName.Split();
+
+            Student newStudent = new Student(nameParts[0], nameParts[1]);
+            context.students.Add(newStudent);
+           
+        }
+    }
+}
---
+++ Student/StudentContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using System.IO;
+
+namespace Student
+{
+    class StudentContext : DbContext
+    {
+
+        // This property corresponds to the table in our database
+        public DbSet<Student> students { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // get the directory the code is being executed from
+            DirectoryInfo ExeDirectory = new DirectoryInfo(AppContext.BaseDirectory);
+
+            // get the base directory for the project
+            DirectoryInfo ProjectBase = ExeDirectory.Parent.Parent.Parent;
+
+            // add 'students.db' to the project directory
+            String DbFile = Path.Combine(ProjectBase.FullName, "student.db");
+
+            // to check what the path of the file is, uncomment the file below
+            Console.WriteLine("using database file :"+DbFile);
+
+            optionsBuilder.UseSqlite("Data Source=" + DbFile);
+        }
+    }
+}
